11cbaaf9a32c94c86a77d7c7bea0fd3a66111017
Sztaszko <majneimejl@gmail.com>
2024-06-08 16:29:04 +0200

    Add displaying products and sets.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Data;
+using System.Data.Common;
 
 namespace GiftSetsWPF{
 
@@ -31,6 +33,67 @@ namespace GiftSetsWPF{
                     UserID = "sa",
                     Password = "db2024"}.ConnectionString);
             
+            DisplayProducts();
+            
+        }
+        private void DisplayProducts() {
+            try {
+                string query = "SELECT * FROM Products";
+                
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, sqlConnection)) {
+                    DataTable setsTable = new DataTable();
+                    adapter.Fill(setsTable);
+                    productsList.DisplayMemberPath = "name";
+                    productsList.SelectedValuePath = "ProductID";
+                    productsList.ItemsSource = setsTable.DefaultView;
+                }
+            } catch (Exception ex) {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void DisplaySets() {
+            try {
+                string query = "SELECT * FROM Sets";
+                
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, sqlConnection)) {
+                    DataTable setsTable = new DataTable();
+                    adapter.Fill(setsTable);
+                    setsList.DisplayMemberPath = "name";
+                    setsList.SelectedValuePath = "SetID";
+                    setsList.ItemsSource = setsTable.DefaultView;
+                }
+            } catch (Exception ex) {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void DisplayProductPrice() {
+            try {
+                string query = "SELECT * FROM Products p WHERE p.ProductID = @ProductID";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlCommand.Parameters.AddWithValue("@ProductID", productsList.SelectedValue);
+
+                sqlConnection.Open();
+                using(SqlDataReader reader = sqlCommand.ExecuteReader()) {
+                    
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(String.Format("{0}, {1}, {2}, {3}",
+                            reader[0], reader[1], reader[2], reader[3]));
+                    }
+                    // productPriceBox.Text = reader[0].ToString();
+                }
+                sqlConnection.Close();
+
+            } catch (Exception ex) {
+                MessageBox.Show(ex.ToString());
+                sqlConnection.Close();
+            }
+        }
+
+        private void productsList_SelectionChanged(object sender, SelectionChangedEventArgs args) {
+            DisplayProductPrice();
         }
     }
 }
