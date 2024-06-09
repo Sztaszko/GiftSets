ba0c6eb74d2cbd88b73aee84aa1eb7c0343e140b
Sztaszko <majneimejl@gmail.com>
2024-06-09 11:22:05 +0200

    Functionality to add the products to create a gift set.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Configuration;
 using System.Data.SqlClient;
 using System.Data;
 using System.Data.Common;
+using System.Reflection.Metadata.Ecma335;
 
 namespace GiftSetsWPF{
 
@@ -23,6 +24,8 @@ namespace GiftSetsWPF{
     public partial class MainWindow : Window
     {
         SqlConnection sqlConnection;
+
+        List<int> createdSetProductsList = new List<int>(); //list of products IDs
         public MainWindow()
         {
             InitializeComponent();
@@ -34,6 +37,10 @@ namespace GiftSetsWPF{
                     Password = "db2024"}.ConnectionString);
             
             DisplayProducts();
+
+            DisplaySets();
+
+            DisplayCreatedSet();
             
         }
         private void DisplayProducts() {
@@ -68,6 +75,34 @@ namespace GiftSetsWPF{
             }
         }
 
+        private void DisplayCreatedSet() {
+            try {
+                if (createdSetProductsList.Any()) {
+                    var parameters = new string[createdSetProductsList.Count];
+                    SqlCommand sqlCommand = new SqlCommand();
+                    
+                    for (int i=0; i<createdSetProductsList.Count; i++) {
+                        parameters[i] = string.Format("@createdSetProductsList{0}", i);
+                        sqlCommand.Parameters.AddWithValue(parameters[i], createdSetProductsList[i]);
+                    }
+
+                    sqlCommand.CommandText = string.Format("SELECT * FROM Products WHERE ProductID in ({0})", string.Join(", ", parameters));
+                    sqlCommand.Connection = sqlConnection;
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand)) {
+                        DataTable createdSetTable = new DataTable();
+                        adapter.Fill(createdSetTable);
+                        createdSetList.DisplayMemberPath = "name";
+                        createdSetList.SelectedValuePath = "SetID";
+                        createdSetList.ItemsSource = createdSetTable.DefaultView;
+                    }
+                }
+                
+            } catch (Exception ex) {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        
         private void DisplayProductPrice() {
             try {
                 string query = "SELECT * FROM Products p WHERE p.ProductID = @ProductID";
@@ -91,8 +126,44 @@ namespace GiftSetsWPF{
             }
         }
 
+        
         private void productsList_SelectionChanged(object sender, SelectionChangedEventArgs args) {
             DisplayProductPrice();
         }
+
+        private void AddToSetClick(object sender, RoutedEventArgs args) {
+             try {
+                // string query = "SELECT * FROM Products p WHERE p.ProductID = @ProductID";
+                // SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                // sqlCommand.Parameters.AddWithValue("@ProductID", productsList.SelectedValue);
+
+                // sqlConnection.Open();
+                // using(SqlDataReader reader = sqlCommand.ExecuteReader()) {
+                    
+                //     if (reader.Read()) {
+                //         var datamap = Enumerable.Range(0, reader.FieldCount).ToDictionary(reader.GetName, reader.GetValue);                     
+                       
+                //         myListBoxItem itm = new myListBoxItem();
+                //         itm.Content = datamap["name"];
+                //         itm.distinct_id = (int) productsList.SelectedValue;
+                //         createdSetList.Items.Add(itm);
+                //     }
+                    
+                // }
+                // sqlConnection.Close();
+
+                createdSetProductsList.Add((int) productsList.SelectedValue);
+                DisplayCreatedSet();
+
+            } catch (Exception ex) {
+                MessageBox.Show(ex.ToString());
+                sqlConnection.Close();
+            }
+        }
+    }
+
+
+    public class myListBoxItem : ListBoxItem {
+        public int distinct_id {get; set;}
     }
 }
